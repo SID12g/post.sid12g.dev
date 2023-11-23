9d34cd0d6c087cfb06d2a1bb98351067e37543ba
SID12g <sid12g@naver.com>
2023-11-23 09:27:18 +0900

    feat : tag bar & tag

--- src/components/home/PostList.tsx
+++ src/components/home/PostList.tsx
@@ -1,22 +1,25 @@
 import styles from '../../styles/PostList.module.css'
 import next_test from '../../../public/next_test.png'
 import Image from 'next/image'
+import Link from 'next/link'
 
 export default function PostList() {
     return (
         <div className={styles.wrap}>
             <p className={styles.header}>📝All Posts (31)</p>
-            <div className={styles.content}>
-                <div className={styles.text}>
-                    <p className={styles.date}>2023년 11월 19일 (일요일)</p>
-                    <p className={styles.title}>Next.js 13.4는 어떤 기능이 추가되었을까? 나는 그것이 진실로 궁금합니다!!</p>
-                    <p className={styles.description}>이전 버전의 Next와 비교하기</p>
-                    <div className={styles.tag}>Next</div>
+            <Link href="" className={styles.link}>
+                <div className={styles.content}>
+                    <div className={styles.text}>
+                        <p className={styles.date}>2023년 11월 19일 (일요일)</p>
+                        <p className={styles.title}>Next.js 13.4는 어떤 기능이 추가되었을까? 나는 그것이 진실로 궁금합니다!!</p>
+                        <p className={styles.description}>이전 버전의 Next와 비교하기</p>
+                        <div className={styles.tag}>Next</div>
+                    </div>
+                    <div className={styles.img_wrap}>
+                        <Image src={next_test} className={styles.image} alt='preview' />
+                    </div>
                 </div>
-                <div className={styles.img_wrap}>
-                    <Image src={next_test} className={styles.image} alt='preview' />
-                </div>
-            </div>
+            </Link>
         </div>
     )
 }
--- src/components/home/Tags.tsx
+++ src/components/home/Tags.tsx
@@ -1,9 +1,19 @@
 import styles from '../../styles/Tags.module.css'
-export default function Tags(){
-    return(
+export default function Tags() {
+    return (
         <div className={styles.wrap}>
             <p className={styles.header}>📌Tags</p>
-            <div className={styles.container}></div>
+            <div className={styles.container}>
+                <div className={styles.scroll}>
+                    <div className={styles.tag}>Next</div>
+                    <div className={styles.tag}>React</div>
+                    <div className={styles.tag}>Computer System</div>
+                    <div className={styles.tag}>Flutter</div>
+                    <div className={styles.tag}>React Native</div>
+                    <div className={styles.tag}>It</div>
+                    <div className={styles.tag}>Algorithm</div>
+                </div>
+            </div>
         </div>
     )
 }

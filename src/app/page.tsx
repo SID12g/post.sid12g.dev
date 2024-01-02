4dc9bfed5cadb13b49fa269ef6a7b4e0d348e530
SID12g <sid12g@naver.com>
2024-01-02 22:19:53 +0900

    test 4 remove cookies

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,14 +4,12 @@ import { Tags } from "@/components/home/Tags";
 import PostList from "@/components/home/PostList";
 import filteredBlogs from "@/utils/getMdxPosts";
 import getTagsWithCounts from "@/utils/getMdxTags";
-import { cookies } from 'next/headers'
 
 export default function Home() {
-  let cookie = cookies().get('mode')
   const tags: unknown[] = getTagsWithCounts
   const getBlogs: unknown[] = filteredBlogs
   return (
-    <div className={cookie != undefined && cookie.value == 'dark' ? styles.wrap_dark : styles.wrap}>
+    <div className={styles.wrap}>
       <SayHello />
       <Tags tags={tags} />
       <PostList getBlogs={getBlogs} />

96fb21600a2a4818e3e7ebcafdba6737a2031073
SID12g <sid12g@naver.com>
2024-01-06 00:10:10 +0900

    fix : add initial value in home

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,19 +4,21 @@ import { Tags } from "@/components/home/Tags";
 import PostList from "@/components/home/PostList";
 import filteredBlogs from "@/utils/getMdxPosts";
 import getTagsWithCounts from "@/utils/getMdxTags";
-import { cookies } from 'next/headers'
+import { cookies } from 'next/headers';
 
 export default function Home() {
-  let cookie: any
-  cookie = cookies().get('mode')
-  const tags: unknown[] = getTagsWithCounts
-  const getBlogs: unknown[] = filteredBlogs
-  
+  // 쿠키 초기화 및 'mode' 속성이 없는 경우 기본값 설정
+  const cookie = cookies().get('mode') || { value: 'light' };
+
+  // 함수 호출을 통한 tags 및 getBlogs 초기화
+  const tags = getTagsWithCounts
+  const getBlogs = filteredBlogs
+
   return (
-    <div className={cookie != '' && cookie.value == 'dark' ? styles.wrap_dark : styles.wrap}>
+    <div className={cookie.value === 'dark' ? styles.wrap_dark : styles.wrap}>
       <SayHello />
       <Tags tags={tags} mode={cookie} />
       <PostList getBlogs={getBlogs} mode={cookie} />
     </div>
-  )
+  );
 }

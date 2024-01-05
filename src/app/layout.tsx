21be521580d4c3f676802ce7fec07c9e034be085
SID12g <sid12g@naver.com>
2024-01-05 23:59:47 +0900

    fix : cookie.value check

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,32 +1,37 @@
-import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
-import '../styles/globals.css'
-import NavBar from '@/components/navbar/NavBar'
-import Copyright from '@/components/Copyright'
-import { cookies } from 'next/headers'
+import type { Metadata } from 'next';
+import { Inter } from 'next/font/google';
+import '../styles/globals.css';
+import NavBar from '@/components/navbar/NavBar';
+import Copyright from '@/components/Copyright';
+import { cookies } from 'next/headers';
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
   title: `sid12g's blog`,
   description: `sid12g's blog`,
-}
+};
 
 export default function RootLayout({
-
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
-  let cookie: any = cookies().get('mode')
-  
+  const cookie: any = cookies().get('mode') || {};
+
   return (
     <html lang="kr">
-      <body style={cookie != '' && cookie.value == 'dark' ? { backgroundColor: '#1d1d1d', color: 'white' } : { backgroundColor: 'white' }}>
+      <body
+        style={
+          cookie && cookie.value === 'dark'
+            ? { backgroundColor: '#1d1d1d', color: 'white' }
+            : { backgroundColor: 'white' }
+        }
+      >
         <NavBar mode={cookie} />
         {children}
         <Copyright />
       </body>
     </html>
-  )
+  );
 }

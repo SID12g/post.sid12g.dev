30c5c76d18011f25f2199a04db7f2000d8110474
SID12g <sid12g@naver.com>
2024-01-06 21:32:52 +0900

    feat : add sitemap link at sayhello

--- src/components/home/SayHello.tsx
+++ src/components/home/SayHello.tsx
@@ -5,7 +5,7 @@ import Link from 'next/link'
 export default function SayHello() {
     return (
         <div className={styles.wrap}>
-            <p className={styles.header}>저의 블로그에 오신걸 환영합니다👋</p>
+            <p className={styles.header}>저의 블로그에 오신걸 환영합니다<Link href='/sitemap.xml' style={{textDecoration: 'none'}}>👋</Link></p>
             <div className={styles.container}>
                 <Image src={information} className={styles.icon} alt="information_icon" />
                 <p className={styles.content}>조성민의 블로그에 방문해 주셔서 감사합니다! 저는 <Link href="https://nextjs.org/">Next.js</Link>와 <Link href="https://reactnative.dev/">React Native</Link>에 관심이 있습니다. 저의 포스트에서 많은 정보를 얻어가시면 좋겠습니다.🐱

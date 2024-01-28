2e959c5a121f59aaf4e149bc0f400dcc6fae6668
SID12g <sid12g@naver.com>
2024-01-28 17:18:13 +0900

    fix: footer color by cookie light -> dark

--- src/components/Copyright.tsx
+++ src/components/Copyright.tsx
@@ -1,13 +1,27 @@
-import Link from "next/link"
-import styles from "../styles/Copyright.module.css"
-import { cookies } from 'next/headers'
+import Link from "next/link";
+import styles from "../styles/Copyright.module.css";
+import { cookies } from "next/headers";
 
 export default function Copyright() {
-    let cookie = cookies().get('mode')
-    var year = new Date().getFullYear()
-    return (
-        <div>
-            <p className={styles.text}>Copyright ⓒ {year} <Link href='https://github.com/SID12g' className={styles.github_link} style={cookie != undefined && cookie.value == 'light' ? {color: 'black'} : {color: 'white'}}>sid12g</Link> All rights reserved.</p>
-        </div>
-    )
+  let cookie = cookies().get("mode");
+  var year = new Date().getFullYear();
+  return (
+    <div>
+      <p className={styles.text}>
+        Copyright ⓒ {year}{" "}
+        <Link
+          href="https://github.com/SID12g"
+          className={styles.github_link}
+          style={
+            cookie != undefined && cookie.value == "dark"
+              ? { color: "white" }
+              : { color: "black" }
+          }
+        >
+          sid12g
+        </Link>{" "}
+        All rights reserved.
+      </p>
+    </div>
+  );
 }

0921f64fe7b8e466ee9b6c5ac26f5d5937227d9b
SID12g <sid12g@naver.com>
2024-02-12 18:04:42 +0900

    fix: light mode 404 color

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,23 +1,22 @@
-import '../styles/globals.css';
-import NavBar from '@/components/navbar/NavBar';
-import Copyright from '@/components/Copyright';
-import { cookies } from 'next/headers';
-
+import "../styles/globals.css";
+import NavBar from "@/components/navbar/NavBar";
+import Copyright from "@/components/Copyright";
+import { cookies } from "next/headers";
 
 export default function RootLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
-  let cookie: any
-  cookie = cookies().get('mode')
+  let cookie: any;
+  cookie = cookies().get("mode");
   return (
     <html lang="kr">
       <body
         style={
-          cookie && cookie.value === 'dark'
-            ? { backgroundColor: '#1d1d1d', color: 'white' }
-            : { backgroundColor: 'white' }
+          cookie && cookie.value === "dark"
+            ? { backgroundColor: "#1d1d1d", color: "white" }
+            : { backgroundColor: "white", color: "black" }
         }
       >
         <NavBar mode={cookie} />

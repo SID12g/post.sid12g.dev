816e2d2b54d3b86f02255980417f830845df191d
SID12g <sid12g@naver.com>
2024-02-29 23:10:37 +0900

    feat: profile

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
+import Profile from "@/components/profile/profile";
 import styles from "./page.module.css";
 import Post from "@/components/post/post";
 
 export default function Home() {
   return (
     <main className={styles.main}>
+      <Profile />
       <Post />
     </main>
   );
---
+++ src/components/profile/profile.tsx
@@ -0,0 +1,22 @@
+import styles from "@/components/profile/profile.module.css";
+import cat from "@/../public/images/cat.svg";
+import Image from "next/image";
+import Link from "next/link";
+
+export default function Profile() {
+  return (
+    <div className={styles.container}>
+      <Image
+        className={styles.image}
+        src={cat}
+        alt="cat"
+        width={128}
+        height={128}
+      />
+      <p className={styles.name}>CHO SUNG MIN</p>
+      <Link className={styles.button} href="mailto:sid12g@naver.com">
+        <p className={styles.email}>sid12g@naver.com</p>
+      </Link>
+    </div>
+  );
+}

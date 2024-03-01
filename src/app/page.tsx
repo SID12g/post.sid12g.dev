e4989c13cafe002e49e86446fadcb49f47e40531
SID12g <sid12g@naver.com>
2024-03-01 13:13:03 +0900

    feat: newpost

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,12 @@
 import Profile from "@/components/profile/profile";
 import styles from "./page.module.css";
 import Post from "@/components/post/post";
+import NewPost from "@/components/newPost/newPost";
 
 export default function Home() {
   return (
     <main className={styles.main}>
+      <NewPost />
       <Profile />
       <Post />
     </main>
---
+++ src/components/newPost/newPost.tsx
@@ -0,0 +1,38 @@
+import styles from "@/components/newPost/newPost.module.css";
+import Image from "next/image";
+import Next from "@/../public/next.png";
+import arrow from "@/../public/icons/arrow.svg";
+
+export default function NewPost() {
+  return (
+    <div className={styles.container}>
+      <div className={styles.info}>
+        <div className={styles.tag}>NEXT</div>
+        <div className={styles.arrow}>
+          <Image
+            className={styles.icon}
+            src={arrow}
+            alt="arrow"
+            width={30}
+            height={30}
+          />
+        </div>
+      </div>
+      <div className={styles.content}>
+        <div className={styles.textWrap}>
+          <p className={styles.description}>
+            13 버전과 비교하며 서버기능 살펴보자!
+          </p>
+          <h1 className={styles.title}>NEXT.JS 14 업데이트 살펴보기</h1>
+        </div>
+        <Image
+          className={styles.image}
+          src={Next}
+          alt="new post"
+          width={720}
+          height={405}
+        />
+      </div>
+    </div>
+  );
+}

01d21d1ebc07b0b5bbdc10813b03e0a9b5b4001b
SID12g <sid12g@naver.com>
2024-03-01 20:03:59 +0900

    feat: footer

---
+++ src/components/footer/footer.tsx
@@ -0,0 +1,30 @@
+import styles from "@/components/footer/footer.module.css";
+import Image from "next/image";
+import Link from "next/link";
+import at from "@/../public/icons/at.svg";
+import github from "@/../public/icons/github.svg";
+import instagram from "@/../public/icons/instagram.svg";
+import wifi from "@/../public/icons/wifi.svg";
+
+export default function Footer() {
+  var year = new Date().getFullYear();
+  return (
+    <div className={styles.container}>
+      <p className={styles.copyright}>ⓒ {year} sid12g All rights reserved.</p>
+      <div className={styles.iconWrap}>
+        <Link className={styles.link} href="mailto:sid12g@naver.com">
+          <Image className={styles.icon} src={at} alt="at" />
+        </Link>
+        <Link className={styles.link} href="https://github.com/sid12g">
+          <Image className={styles.icon} src={github} alt="github" />
+        </Link>
+        <Link className={styles.link} href="https://instagram.com/clwm_222">
+          <Image className={styles.icon} src={instagram} alt="instagram" />
+        </Link>
+        <Link className={styles.link} href="/sitemap.xml">
+          <Image className={styles.icon} src={wifi} alt="wifi" />
+        </Link>
+      </div>
+    </div>
+  );
+}

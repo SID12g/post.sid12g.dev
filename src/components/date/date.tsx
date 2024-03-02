4785b51d0a0121521fe80915561a5d6646f6509f
SID12g <sid12g@naver.com>
2024-03-03 02:01:56 +0900

    fix: date content:telephone=no

--- src/components/date/date.tsx
+++ src/components/date/date.tsx
@@ -1,5 +1,9 @@
 import styles from "@/components/date/date.module.css";
 
 export default function Date({ date }: { date: string }) {
-  return <div className={styles.date}>{date}</div>;
+  return (
+    <div className={styles.date} content="telephone=no">
+      {date}
+    </div>
+  );
 }

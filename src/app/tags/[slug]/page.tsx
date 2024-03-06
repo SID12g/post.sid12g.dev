e5a85b900a170f35fe2f13566a940e7f1db28d50
SID12g <sid12g@naver.com>
2024-03-06 10:29:36 +0900

    feat: tags page

--- src/app/tags/[slug]/page.tsx
+++ src/app/tags/[slug]/page.tsx
@@ -1,3 +1,37 @@
-export default function Tags() {
-  return <div>tags</div>;
+import styles from "@/app/tags/[slug]/page.module.css";
+import Link from "next/link";
+
+export default function Tags({ params }: { params: { slug: string } }) {
+  console.log(params);
+  const tags = [
+    { tag: "Next", count: 1, link: "next" },
+    { tag: "React", count: 2, link: "react" },
+    { tag: "Firebase", count: 3, link: "firebase" },
+    { tag: "React Native", count: 7, link: "react-native" },
+    { tag: "React", count: 2, link: "react" },
+    { tag: "Firebase", count: 3, link: "firebase" },
+    { tag: "React Native", count: 7, link: "react-native" },
+  ];
+  function getLink() {
+    return tags.find((tag) => tag.link == params.slug)?.tag;
+  }
+  getLink();
+  return (
+    <main className={styles.main}>
+      <div>
+        <h2 className={styles.nowTag}>
+          {params.slug === "all" ? "ALL POSTS." : getLink() + "."}
+        </h2>
+        <div className={styles.tagsWrap}>
+          {tags.map((tag, index) => (
+            <Link href={"/tags/" + tag.link} key={index}>
+              <p className={styles.tag}>
+                {tag.tag} ({tag.count})
+              </p>
+            </Link>
+          ))}
+        </div>
+      </div>
+    </main>
+  );
 }

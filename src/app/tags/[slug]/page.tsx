6075899058e72d6bff028ddffaff3e9e629e451f
SID12g <sid12g@naver.com>
2024-03-06 22:53:01 +0900

    feat: posts in tag page

--- src/app/tags/[slug]/page.tsx
+++ src/app/tags/[slug]/page.tsx
@@ -1,4 +1,5 @@
 import styles from "@/app/tags/[slug]/page.module.css";
+import Post from "@/components/post/post";
 import Link from "next/link";
 import { redirect } from "next/navigation";
 
@@ -26,6 +27,72 @@ export default function Tags({ params }: { params: { slug: string } }) {
     return tags.find((tag) => tag.link == params.slug)?.count;
   }
   getLink();
+  var list = [
+    {
+      title: "1번 글 입니다.",
+      description: "이건 1번 글의 설명입니다.",
+      date: "2000-00-00",
+    },
+    {
+      title: "2번 글 입니다.",
+      description: "이건 2번 글의 설명입니다.",
+      date: "2000-00-00",
+    },
+    {
+      title: "3번 글 입니다.",
+      description: "이건 3번 글의 설명입니다.",
+      date: "2000-00-00",
+    },
+    {
+      title: "4번 글 입니다.",
+      description: "이건 4번 글의 설명입니다.",
+      date: "2000-00-00",
+    },
+    {
+      title: "5번 글 입니다.",
+      description: "이건 5번 글의 설명입니다.",
+      date: "2000-00-00",
+    },
+    {
+      title: "6번 글 입니다.",
+      description: "이건 6번 글의 설명입니다.",
+      date: "2000-00-00",
+    },
+  ];
+  var list1 = [
+    {
+      title: "1번 글 입니다.",
+      description: "이건 1번 글의 설명입니다.",
+      date: "2000-00-00",
+    },
+    {
+      title: "3번 글 입니다.",
+      description: "이건 1번 글의 설명입니다.",
+      date: "2000-00-00",
+    },
+    {
+      title: "5번 글 입니다.",
+      description: "이건 1번 글의 설명입니다.",
+      date: "2000-00-00",
+    },
+  ];
+  var list2 = [
+    {
+      title: "2번 글 입니다.",
+      description: "이건 2번 글의 설명입니다.",
+      date: "2000-00-00",
+    },
+    {
+      title: "4번 글 입니다.",
+      description: "이건 1번 글의 설명입니다.",
+      date: "2000-00-00",
+    },
+    {
+      title: "6번 글 입니다.",
+      description: "이건 1번 글의 설명입니다.",
+      date: "2000-00-00",
+    },
+  ];
   return (
     <main className={styles.main}>
       <div>
@@ -47,6 +114,41 @@ export default function Tags({ params }: { params: { slug: string } }) {
               </p>
             </Link>
           ))}
+        </div>
+        <div className={styles.bottom}>
+          <div className={styles.singlePost}>
+            {list.map((post, index) => (
+              <Post
+                title={post.title}
+                description={post.description}
+                date={post.date}
+                key={index}
+              />
+            ))}
+          </div>
+          <div className={styles.doublePost}>
+            <div style={{ width: "50%" }}>
+              {list1.map((post, index) => (
+                <Post
+                  title={post.title}
+                  description={post.description}
+                  date={post.date}
+                  key={index}
+                />
+              ))}
+            </div>
+            <div style={{ width: 60 }}></div>
+            <div style={{ width: "50%" }}>
+              {list2.map((post, index) => (
+                <Post
+                  title={post.title}
+                  description={post.description}
+                  date={post.date}
+                  key={index}
+                />
+              ))}
+            </div>
+          </div>
         </div>
       </div>
     </main>

72191d84c4fd52b4a69591cc6c6d206aec9a3cab
SID12g <sid12g@naver.com>
2024-03-06 18:29:10 +0900

    feat: tag == undefined -> redirect(not-found)

--- src/app/tags/[slug]/page.tsx
+++ src/app/tags/[slug]/page.tsx
@@ -1,9 +1,11 @@
 import styles from "@/app/tags/[slug]/page.module.css";
 import Link from "next/link";
+import { redirect } from "next/navigation";
 
 export default function Tags({ params }: { params: { slug: string } }) {
   console.log(params);
   const tags = [
+    { tag: "All", count: 10, link: "all" },
     { tag: "Next", count: 1, link: "next" },
     { tag: "React", count: 2, link: "react" },
     { tag: "Firebase", count: 3, link: "firebase" },
@@ -13,7 +15,11 @@ export default function Tags({ params }: { params: { slug: string } }) {
     { tag: "React Native", count: 7, link: "react-native" },
   ];
   function getLink() {
-    return tags.find((tag) => tag.link == params.slug)?.tag;
+    if (tags.find((tag) => tag.link == params.slug)?.tag === undefined) {
+      redirect("/not-found");
+    } else {
+      return tags.find((tag) => tag.link == params.slug)?.tag;
+    }
   }
   getLink();
   return (
@@ -24,7 +30,11 @@ export default function Tags({ params }: { params: { slug: string } }) {
         </h2>
         <div className={styles.tagsWrap}>
           {tags.map((tag, index) => (
-            <Link href={"/tags/" + tag.link} key={index}>
+            <Link
+              className={styles.link}
+              href={"/tags/" + tag.link}
+              key={index}
+            >
               <p className={styles.tag}>
                 {tag.tag} ({tag.count})
               </p>
--- src/components/post/post.tsx
+++ src/components/post/post.tsx
@@ -3,6 +3,7 @@ import Image from "next/image";
 import Next from "@/../public/next.png";
 import Date from "../date/date";
 import Tag from "../tag/tag";
+import Link from "next/link";
 
 export default function Post({
   title,
@@ -15,13 +16,15 @@ export default function Post({
 }) {
   return (
     <div className={styles.container}>
-      <Image className={styles.image} src={Next} alt="Thumbnail" />
-      <h2 className={styles.title}>{title}</h2>
-      <p className={styles.description}>{description}</p>
-      <div className={styles.info}>
-        <Date date={date} />
-        <Tag border="black" background="black" color="white" tag="NEXT" />
-      </div>
+      <Link className={styles.link} href="/posts/a">
+        <Image className={styles.image} src={Next} alt="Thumbnail" />
+        <h2 className={styles.title}>{title}</h2>
+        <p className={styles.description}>{description}</p>
+        <div className={styles.info}>
+          <Date date={date} />
+          <Tag border="black" background="black" color="white" tag="NEXT" />
+        </div>
+      </Link>
     </div>
   );
 }

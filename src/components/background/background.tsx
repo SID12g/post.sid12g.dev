ac0a8da85d897988d70be664f5febf960690bcf7
SID12g <sid12g@naver.com>
2024-07-07 22:22:08 +0900

    feat: background data

--- src/components/background/background.tsx
+++ src/components/background/background.tsx
@@ -1,23 +1,27 @@
 import styles from "@/components/background/background.module.css";
 import Image from "next/image";
-import background from "@/../public/background.jpg";
+import { imageData } from "@/utils/data";
+import Link from "next/link";
 
 export default function Background({ location }: { location: string }) {
+  const randomNum = Math.floor(Math.random() * 58);
+
   return (
-    <div className={styles.background}>
+    <Link href={imageData[randomNum].links.html} className={styles.background}>
       <Image
+        priority={true}
         className={styles.background_image}
-        src={background}
-        width={2160}
-        height={1440}
+        src={imageData[randomNum].urls.full}
+        width={1100}
+        height={400}
         alt="background"
       />
       <div className={styles.message_wrap}>
         <p className={styles.message}>
-          사실 명언이란, 힘있는 자의 허세일 뿐이다.
+          인생의 정답이 없다는 말이 오답을 적어도 된다는 뜻은 아닐테니
         </p>
-        <p className={styles.message_author}>문석호</p>
+        <p className={styles.message_author}>아리안 시로네</p>
       </div>
-    </div>
+    </Link>
   );
 }

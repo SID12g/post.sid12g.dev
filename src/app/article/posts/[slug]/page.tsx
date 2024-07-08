4be55376aef75231da5f2a9b47f088d43e3563a4
SID12g <sid12g@naver.com>
2024-07-08 09:58:43 +0900

    feat: post -> article component

--- src/app/article/posts/[slug]/page.tsx
+++ src/app/article/posts/[slug]/page.tsx
@@ -1,42 +1,7 @@
-import styles from "@/app/article/posts/[slug]/page.module.css";
-import { MDXRemote } from "next-mdx-remote/rsc";
 import fs from "fs";
 import path from "path";
 import matter from "gray-matter";
-import Image from "next/image";
-import Link from "next/link";
-import { techTags } from "@/utils/getTags";
-import rehypeSlug from "rehype-slug";
-import remarkToc from "remark-toc";
-import rehypeAutolinkHeadings from "rehype-autolink-headings";
-import rehypePrettyCode from "rehype-pretty-code";
-import Comments from "@/components/comments/comments";
-import rehypeCodeTitles from "rehype-code-titles";
-
-const options: any = {
-  mdxOptions: {
-    remarkPlugins: [remarkToc],
-    rehypePlugins: [
-      rehypeSlug,
-      rehypeCodeTitles,
-      [
-        rehypePrettyCode,
-        {
-          theme: "github-dark",
-        },
-      ],
-      [
-        rehypeAutolinkHeadings,
-        {
-          behavior: "wrap",
-          properties: {
-            className: [styles.anchor],
-          },
-        },
-      ],
-    ],
-  },
-};
+import Article from "@/components/article/article";
 
 function getPost({ slug }: { slug: string }) {
   const markdownFile = fs.readFileSync(
@@ -74,30 +39,5 @@ export async function generateMetadata({ params }: any) {
 
 export default function Post({ params }: { params: { slug: string } }) {
   const props = getPost(params);
-  function getLink() {
-    return techTags.find((tags) => tags.tag == props.frontMatter.tag)?.link;
-  }
-  return (
-    <article className={styles.article}>
-      <Image
-        src={props.frontMatter.image}
-        className={styles.image}
-        alt="preview"
-        width={1920}
-        height={1080}
-      />
-      <h1 className={styles.title}>{props.frontMatter.title}</h1>
-      <div className={styles.information_wrap}>
-        <Link className={styles.tag} href={"/tech/" + getLink()}>
-          {props.frontMatter.tag}
-        </Link>
-        <p className={styles.date}>{props.frontMatter.date}</p>
-      </div>
-      <p className={styles.description}>{props.frontMatter.description}</p>
-      <div className={styles.content}>
-        <MDXRemote source={props.content} options={options} />
-      </div>
-      <Comments />
-    </article>
-  );
+  return <Article props={props} />;
 }
---
+++ src/components/article/article.tsx
@@ -0,0 +1,65 @@
+import styles from "@/components/article/article.module.css";
+import { MDXRemote } from "next-mdx-remote/rsc";
+import Image from "next/image";
+import Link from "next/link";
+import { techTags } from "@/utils/getTags";
+import rehypeSlug from "rehype-slug";
+import remarkToc from "remark-toc";
+import rehypeAutolinkHeadings from "rehype-autolink-headings";
+import rehypePrettyCode from "rehype-pretty-code";
+import Comments from "@/components/comments/comments";
+import rehypeCodeTitles from "rehype-code-titles";
+
+const options: any = {
+  mdxOptions: {
+    remarkPlugins: [remarkToc],
+    rehypePlugins: [
+      rehypeSlug,
+      rehypeCodeTitles,
+      [
+        rehypePrettyCode,
+        {
+          theme: "github-light",
+        },
+      ],
+      [
+        rehypeAutolinkHeadings,
+        {
+          behavior: "wrap",
+          properties: {
+            className: [styles.anchor],
+          },
+        },
+      ],
+    ],
+  },
+};
+
+export default function Article({ props }: { props: any }) {
+  function getLink() {
+    return techTags.find((tags) => tags.tag == props.frontMatter.tag)?.link;
+  }
+  return (
+    <article className={styles.article}>
+      <Image
+        src={props.frontMatter.image}
+        className={styles.image}
+        alt="preview"
+        width={1920}
+        height={1080}
+      />
+      <h1 className={styles.title}>{props.frontMatter.title}</h1>
+      <div className={styles.information_wrap}>
+        <Link className={styles.tag} href={"/tech/" + getLink()}>
+          {props.frontMatter.tag}
+        </Link>
+        <p className={styles.date}>{props.frontMatter.date}</p>
+      </div>
+      <p className={styles.description}>{props.frontMatter.description}</p>
+      <div className={styles.content}>
+        <MDXRemote source={props.content} options={options} />
+      </div>
+      <Comments />
+    </article>
+  );
+}

80b368ae0be554f7477cd37a90de22f406de31d0
Sungmin Cho <sid12g@naver.com>
2025-06-26 00:14:40 +0000

    fix: cloudflare setting workers -> pages

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,6 @@
+import { setupDevPlatform } from '@cloudflare/next-on-pages/next-dev';
+ 
 /** @type {import('next').NextConfig} */
-
 const nextConfig = {
   images: {
     remotePatterns: [
@@ -11,4 +12,9 @@ const nextConfig = {
   },
 };
 
+ 
+ if (process.env.NODE_ENV === 'development') {
+   await setupDevPlatform();
+ }
+ 
 export default nextConfig;
